6962c01e039415241b43d4c05712c4a96afe458a
abovet <audrey.bovet@ensae.fr>
2023-03-09 21:49:56 +0000

    On essaye de faire le test de la question 12

--- delivery_network/main.py
+++ delivery_network/main.py
@@ -1,8 +1,8 @@
-from graph import Graph, graph_from_file
+from graph import Graph, graph_from_file, add_edge
 
 
 data_path = "input/"
-file_name = "network.01.in"
+file_name = "network.02.in"
 
 g = graph_from_file(data_path + file_name)
 print(g)
@@ -13,16 +13,19 @@ def find(parent, x):
     return x
     
 
-def kurskal(g):
+def kruskal(g):
     g_mst=Graph() #on initialise le graphe à renvoyer
+    mst_edges=[]
 
     aretes=[]#initialisation de la liste des puissances
     
-    for node in nodes:#Création de la liste des puissances en parcourant toutes les arrêtes du graphe
+    for node in g.nodes:#Création de la liste des puissances en parcourant toutes les arrêtes du graphe
         for e in g[node]:
             ar = e.insert(0,node) #on crée l'arête de la forme (noeud1, noeud2, power, dist)
             aretes.append(ar)# on ajoute l'arête à la liste des arêtes
     aretes.sort(key=lambda x : x[1])#tri de la liste
+
+    #En triant les arêtes par puissance, on est sûr de tester les arêtes donnant l'arbre couvrant minimum
 
     parent = list(range(g.nb_nodes)) #on initialise la liste représentant les sous-ensembles, chaque noeud est parent de lui-même à l'initialisation
 
@@ -31,15 +34,37 @@ def kurskal(g):
         #est représenté par un noeud référent appelé parent.
         #On renome donc les noeuds pour que le nom du noeud à l'initialisation corresponde à son indice dans la liste parent
         #C'est pour cela qu'on soustrait 1 à chaque noeud
-        n1-1 = ar[0] 
-        n2-1=ar[1]
+        n1= ar[0] -1 
+        n2=ar[1] -1
+        a=ar[0]
+        
+        
+        parent_n1=find(parent, n1)
+        parent_n2=find(parent, n2)
+
+        #on regarde si les sommets de l'arête appartiennent au  même sous-ensemble
+        #S'ils ont le même parent, ils appartiennent au même sous-ensemble, relier les deux somments formerait un cycle
+        #Donc on n'ajoute pas cette arête à l'arbe couvrant de poids minimal
+        if parent_n1 != parent_n2 :
+            #S'ils n'appartiennent au  même sous ensemble, on fusionne ces ensembles
+            parent[parent_n1] = parent_n2 #donc l'ensemble 1 a maintenant pour parent le parent de l'ensemble 2
+            
+        mst_edges.append(ar)
+    g_mst = Graph(g.nodes())
+
+    for i in range (0,len(mst_edges)):
+        ar=mst[i]
+        a=ar[0]
+        b=ar[1]
         p=ar[2] #puissance de l'arête
         if len(ar)>3:#si l'arête a une puissance, on l'ajoute
             dist=ar[3]
-        
+        g_mst.add_edge(a, b , p, dist)
+
+    return g_mst
         
         
 
-    mst=[]
+    
 
     
---
+++ tests/test_s2q12_kruskal.py
@@ -0,0 +1,21 @@
+# This will work if ran from the root folder.
+import sys 
+sys.path.append("delivery_network")
+
+from graph import graph_from_file
+import unittest   # The test framework
+
+
+
+class Test_MinimalPower(unittest.TestCase):
+    def test_network02(self):
+        g = graph_from_file("input/network.02.in")
+        import main
+        main.kruskal(g)
+        
+        self.assertEqual(kruskal(g),{1:[(4,4)], 4: [(3,4), (1,4)], 3: [(4,4), (2,4)], 2: (3,4)})
+
+    
+
+if __name__ == '__main__':
+    unittest.main()
