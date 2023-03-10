529a5623331e31d9b6f48459652216e8a0e8a29c
aduclert <alexia.duclert@ensae.fr>
2023-03-10 15:23:40 +0000

    test

--- tests/test_s2q12_kruskal.py
+++ tests/test_s2q12_kruskal.py
@@ -11,8 +11,9 @@ class Test_MinimalPower(unittest.TestCase):
     def test_network02(self):
         g = graph_from_file("input/network.02.in")
         import main
-        main.kruskal(g)
-        
+        m= main.kruskal(g)
+        print('Hello')
+        print(m)
         self.assertEqual(main.kruskal(g),{1:[(4,4)], 4: [(3,4), (1,4)], 3: [(4,4), (2,4)], 2: (3,4)})
 
     
