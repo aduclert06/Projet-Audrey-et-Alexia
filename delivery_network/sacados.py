6954ab7da9f6f735adbde39874e586ca59c97de7
abovet <audrey.bovet@ensae.fr>
2023-04-02 15:00:37 +0000

    ajouts commentaires

--- delivery_network/sacados.py
+++ delivery_network/sacados.py
@@ -3,6 +3,7 @@ from kruskal_min_power import*
 
 #enlever les camions inutiles (plus cher que d'autres camion avec une plus grande puissance)
 
+#récupérer liste des camions :
 
 def catalogue_from_file_in(x):
     ''' Cette fonction permet de renvoyer le catalogue de camion sous forme de liste à partir du fichier trucks.x.in.
@@ -27,11 +28,12 @@ def catalogue_from_file_in(x):
             res.append((puissance, cout))
     return(res)
 
+
 def catalogue_from_file_out(x):
-    ''' Cette fonction permet de renvoyer le catalogue de camion sous forme de liste à partir du fichier trucks.x.out.
+    ''' Cette fonction permet de renvoyer le catalogue de camions utiles sous forme de liste à partir du fichier trucks_utile.x.out.
 
         Args: x(int): numéro du fichier trucks.x.in
-        returns : res(list) : liste des couples (puissance, cout) de chaque camion du fichier trucks
+        returns : res(list) : liste des couples (puissance, cout) de chaque camion utile du fichier trucks_utile
     '''
 
     filename="input/trucks_utile."+ str(x) + ".out"
@@ -49,6 +51,8 @@ def catalogue_from_file_out(x):
 
             res.append((puissance, cout))
     return(res)
+
+#récupérer les routes sous forme de liste
 
 def routes_from_file_out(x):
     ''' Cette fonction permet de renvoyer le fichier route out sous forme de liste
@@ -77,9 +81,11 @@ def routes_from_file_out(x):
 
 #print(routes_from_file_out(1))
 
+#enlever les camions inutiles du catalogue
+
 def catalogue_utile(x):
     '''Cette fonction enlève les camions "inutiles", c'est-à-dire ceux ayant une puissance 
-    inférieure à un autre camion moins cher. Elle crée un fichier text trucks.x.out avec les camions utiles
+    inférieure à un autre camion moins cher. Elle crée un fichier text trucks_utile.x.out avec les camions utiles
     args : x(int): numero du fichier trucks
     returns : None
 
@@ -117,6 +123,8 @@ def catalogue_utile(x):
 c=[(1000,99),(2000, 294), (3000,365), (4000,382),(5000,295)]
 
 #print(catalogue_utile(0))
+
+#Se rapporter au problème du sac à dos. A chaque route on associe un camion avec une certaine puissance et un certain  prix
 
 def appariement(x_trucks,y_routes):
     '''Cette fonction choisit le camion le moins cher pour chaque trajet du fichier routes.y_routes_out.out
@@ -178,7 +186,10 @@ def appariement(x_trucks,y_routes):
 
     return(appariement)
 
-print(appariement(2,4))
+#print(appariement(2,4))
+
+#fonction sac à dos
+
 
 
 
